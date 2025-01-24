abf51be7f926470bc5ed9a2752a145e1366012b3
DiegoWearden <diegowearden@utexas.edu>
2025-01-24 04:21:20 -0600

    environment

---
+++ src/kernel.cpp
@@ -0,0 +1,72 @@
+#include "uart.h"
+#include "utils.h"
+#include "printf.h"
+#include "percpu.h"
+#include "stdint.h"
+#include "entry.h"
+#include "core.h"
+
+struct Stack {
+    static constexpr int BYTES = 4096;
+    uint64_t bytes[BYTES] __attribute__ ((aligned(16)));
+};
+
+PerCPU<Stack> stacks;
+
+static bool smpInitDone = false;
+
+extern "C" uint64_t pickKernelStack(void) {
+    return (uint64_t) &stacks.forCPU(smpInitDone ? getCoreID() : 0).bytes[Stack::BYTES];
+}
+
+void print_ascii_art() {
+    printf("\n");
+    printf("                                                                                     \n");
+    printf("      # ###          #######                                                /       \n");
+    printf("    /  /###        /       ###                           #                #/        \n");
+    printf("   /  /  ###      /         ##      #                   ###               ##        \n");
+    printf("  /  ##   ###     ##        #      ##                    #                ##        \n");
+    printf(" /  ###    ###     ###             ##                                     ##        \n");
+    printf("##   ##     ##    ## ###         ######## ###  /###    ###        /###    ##  /##   \n");
+    printf("##   ##     ##     ### ###      ########   ###/ #### /  ###      / ###  / ## / ###  \n");
+    printf("##   ##     ##       ### ###       ##       ##   ###/    ##     /   ###/  ##/   ### \n");
+    printf("##   ##     ##         ### /##     ##       ##           ##    ##         ##     ## \n");
+    printf("##   ##     ##           #/ /##    ##       ##           ##    ##         ##     ## \n");
+    printf(" ##  ##     ##            #/ ##    ##       ##           ##    ##         ##     ## \n");
+    printf("  ## #      /              # /     ##       ##           ##    ##         ##     ## \n");
+    printf("   ###     /     /##        /      ##       ##           ##    ###     /  ##     ## \n");
+    printf("    ######/     /  ########/       ##       ###          ### /  ######/   ##     ## \n");
+    printf("      ###      /     #####          ##       ###          ##/    #####     ##    ## \n");
+    printf("               |                                                                 /  \n");
+    printf("                \\)                                                              /   \n");
+    printf("                                                                               /    \n");
+    printf("                                                                              /     \n");
+}
+
+
+
+
+extern "C" void kernel_init() {
+    if(getCoreID() == 0){
+        uart_init();
+        init_printf(nullptr, uart_putc_wrapper);
+        printf("printf initialized!!!\n");
+        print_ascii_art();
+        // Initialize MMU page tables
+        // Initialize heap
+        smpInitDone = true;
+        wake_up_cores();
+    }
+
+    // Enable MMU (all cores must set the enable MMU bit to 1)
+
+    // this line here will cause race conditions between cores
+    printf("Hi, I'm core %d\n", getCoreID());
+
+    if(getCoreID() == 0){
+        while (1) {
+            uart_putc(uart_getc()); // will allow you to type letters through UART
+        }
+    }
+}
+
