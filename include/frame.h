75fbb786228e710068b1929e7aa9e71abeeb7e1b
alogx11 <alogx11@gmail.com>
2025-03-04 17:43:19 -0600

    removed frame2

--- include/frame.h
+++ include/frame.h
@@ -11,8 +11,6 @@
 
 void create_frame_table(uintptr_t start, int size);
 
-void alloc_frame2(int flags, Function<void(int)> w);
-
 void alloc_frame(int flags, Function<void(uint64_t)> w);
 
 bool free_frame(uintptr_t frame_addr);
