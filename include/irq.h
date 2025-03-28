b205123c4d5b318bfe5d7caf8c76bbcd95a61984
alogx11 <alogx11@gmail.com>
2025-03-28 12:00:48 -0500

    added function name to irq.s to start initializing gic

--- include/irq.h
+++ include/irq.h
@@ -2,11 +2,12 @@
 #define _IRQ_H
 
 void enable_interrupt_controller(void);
-void gic_init(void);
+// void gic_init(void);
 
 extern "C" void handle_irq(void);
 extern "C" void enable_irq(void);
 extern "C" void disable_irq(void);
 extern "C" void rearm_timer(void);
+extern "C" void gic_init(void);
 
 #endif /*_IRQ_H */
