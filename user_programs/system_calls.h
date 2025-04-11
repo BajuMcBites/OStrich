d692a4d4d75579c335ef65cb0319fc69627a8cca
Arvind Ganesh <arvind.cganesh@gmail.com>
2025-04-10 22:51:57 -0500

    clean up userprog directory

--- user_programs/system_calls.h
+++ user_programs/system_calls.h
@@ -1,23 +1,27 @@
-#ifndef _SYS__H_
-#define _SYS___H_
+#ifndef _SYSTEM_CALLS_H_
+#define _SYSTEM_CALLS_H_
 
-#define SYS_EXIT 0
-#define SYS_CLOSE 1
-#define SYS_ENV 2
-#define SYS_EXEC 3
-#define SYS_FORK 4
-#define SYS_FSTAT 5
-#define SYS_GETPID 6
-#define SYS_ISATTY 7
-#define SYS_KILL 8
-#define SYS_LINK 9
-#define SYS_LSEEK 10
-#define SYS_OPEN 11
-#define SYS_READ 12
-#define SYS_STAT 13
-#define SYS_UNLINK 14
-#define SYS_WAIT 15
-#define SYS_WRITE 16
-#define SYS_TIME 17
+// Newlib system calls numbers.
+#define NEWLIB_EXIT 0
+#define NEWLIB_CLOSE 1
+#define NEWLIB_ENV 2
+#define NEWLIB_EXEC 3
+#define NEWLIB_FORK 4
+#define NEWLIB_FSTAT 5
+#define NEWLIB_GETPID 6
+#define NEWLIB_ISATTY 7
+#define NEWLIB_KILL 8
+#define NEWLIB_LINK 9
+#define NEWLIB_LSEEK 10
+#define NEWLIB_OPEN 11
+#define NEWLIB_READ 12
+#define NEWLIB_STAT 13
+#define NEWLIB_UNLINK 14
+#define NEWLIB_WAIT 15
+#define NEWLIB_WRITE 16
+#define NEWLIB_TIME 17
+#define NEWLIB_SBRK 999
+
+// TODO: Later, add Linux system call #'s here.
 
 #endif
