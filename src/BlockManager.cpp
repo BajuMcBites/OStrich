986688bafad03cf4fab4f6eae5d44a60669c1703
Arvind Ganesh <arvind.cganesh@gmail.com>
2025-04-11 21:50:09 -0500

    fs compatibility stuff

---
+++ src/BlockManager.cpp
@@ -0,0 +1,9 @@
+#include "../filesystem/interface/BlockManager.h"
+
+bool BlockManager::readBlock(size_t blockIndex, uint8_t* buffer) {
+    return true;
+}
+
+bool BlockManager::writeBlock(size_t blockIndex, const uint8_t* buffer) {
+    return true;
+}
