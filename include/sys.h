502d08c6a740bdc13cb851712357107be59cb0b1
alogx11 <alogx11@gmail.com>
2025-04-16 23:06:14 -0500

    moved KernelEntry frame to .h

--- include/sys.h
+++ include/sys.h
@@ -1,7 +1,18 @@
 #ifndef _SYS__H_
-#define _SYS___H_
+#define _SYS__H_
 
-extern "C" void syscall_handler(void* syscall_frame);
+// KernelEntryFrame structure.
+// X[0] - return value
+struct KernelEntryFrame {
+    union {
+        /* X[0] ... X[5] - arguments
+        once function is called, X[0] will be the return value.
+        */
+        uint64_t X[31];  // 0 ... 30
+    };
+};
+
+extern "C" void syscall_handler(KernelEntryFrame* frame);
 
 void invoke_handler(int opcode);
 
