1e205945a5a79ef1e561e672e4c34b4a8041274a
bleh05 <qinxiaoying@gmail.com>
2025-04-17 13:14:30 -0500

    add comments

--- src/sys.cpp
+++ src/sys.cpp
@@ -136,11 +136,8 @@ int newlib_handle_close(SyscallFrame* frame) {
 
 int newlib_handle_exec(SyscallFrame* frame) {
     char* pathname = (char*)frame->X[0];
-    char* pathname_copy = (char*)kmalloc(K::strlen(pathname));
-    K::memcpy(pathname_copy, pathname, K::strlen(pathname));
     char** argv = (char**)frame->X[1];
-    printf("getting %s\n", pathname_copy);
-    int elf_index = get_ramfs_index(pathname_copy);
+    int elf_index = get_ramfs_index(pathname);
     if (elf_index == -1) {
         printf("invalid file name!\n");
         return 1;
@@ -149,27 +146,23 @@ int newlib_handle_exec(SyscallFrame* frame) {
     tcb->state = TASK_STOPPED;
     PCB* pcb = tcb->pcb;
     int pid = pcb->pid;
-    printf("%x, argv[0]\n", *argv[0]);
+    
+    // calculate argc
     int argc = 0;
-    for (; *argv[argc] != 0; argc++) {
-    }
+    for (; *argv[argc] != 0; argc++);
     
-    uint64_t argv_sz = (uint64_t)(&argv[argc]) - (uint64_t)argv;
-    char** argv_copy = (char**)kmalloc(argv_sz);
-    K::memcpy(argv_copy, argv, argv_sz);
-    argv = argv_copy;
-    
+    // load elf file
     const int sz = ramfs_size(elf_index);
     char* buffer = (char*)kmalloc(sz);
     ramfs_read(buffer, 0, sz, elf_index);
     Semaphore* sema = new Semaphore(1);
     void* new_pc = elf_load((void*)buffer, pcb, sema);
     
+    // set up stack
     uint64_t sp = 0x0000fffffffff000;
     pcb->page_table->use_page_table();
     K::memset((void*)(sp - PAGE_SIZE), 0, PAGE_SIZE);
     uint64_t addrs[argc];
-    printf("%d argc\n", argc);
     for (int i = argc - 1; i >= 0; --i) {
         int len = K::strlen(argv[i]) + 1;
         sp -= len;
@@ -205,6 +198,7 @@ int newlib_handle_exec(SyscallFrame* frame) {
 
 int newlib_handle_fork(SyscallFrame* frame) {
     UserTCB* curr_tcb = get_running_user_tcb(getCoreID()); 
+    // copy tcb attributes 
     UserTCB* new_tcb = new UserTCB();
     K::memcpy((void*)new_tcb, (void*)curr_tcb, sizeof(UserTCB));
     K::memcpy((void*)&(new_tcb->context), (void*)frame->X, sizeof(frame->X));
@@ -264,15 +258,17 @@ int newlib_handle_fork(SyscallFrame* frame) {
         sema->up();
     });
     curr_tcb->pcb->page_table->use_page_table();
+
+    // registers specified to put the pc and sp back to
     new_tcb->context.pc = (uint64_t)frame->X[30];
     new_tcb->context.sp = (uint64_t)frame->X[29];
     new_tcb->pcb = pcb;
+
+    // fork returns 0 to child process
     set_return_value(new_tcb, 0);
     sema->down([=]() {
         queue_user_tcb(new_tcb);
     });
-    // set_return_value(new_tcb, 0);
-    // queue_user_tcb(new_tcb);
     return pcb->pid;
 }
 
