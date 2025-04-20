1b50948e3a3b9e78937028751e15788a2aea5c4c
DiegoWearden <diegowearden@utexas.edu>
2025-04-19 20:42:23 -0500

    fixed user paging fb bugs, working on getting fb for doom

--- Arm32_64_USB/Build/rpi-SmartStart.d
+++
@@ -1,6 +0,0 @@
-Build/rpi-SmartStart.o: \
- /home/diego/Desktop/OStrich/Arm32_64_USB/rpi-SmartStart.c \
- /home/diego/Desktop/OStrich/Arm32_64_USB/Font8x16.h \
- /home/diego/Desktop/OStrich/Arm32_64_USB/rpi-smartstart.h
-/home/diego/Desktop/OStrich/Arm32_64_USB/Font8x16.h:
-/home/diego/Desktop/OStrich/Arm32_64_USB/rpi-smartstart.h:
--- Arm32_64_USB/Build/rpi-usb.d
+++
@@ -1,7 +0,0 @@
-Build/rpi-usb.o: /home/diego/Desktop/OStrich/Arm32_64_USB/rpi-usb.c \
- /home/diego/Desktop/OStrich/Arm32_64_USB/rpi-smartstart.h \
- /home/diego/Desktop/OStrich/Arm32_64_USB/rpi-usb.h \
- /home/diego/Desktop/OStrich/Arm32_64_USB/emb-stdio.h
-/home/diego/Desktop/OStrich/Arm32_64_USB/rpi-smartstart.h:
-/home/diego/Desktop/OStrich/Arm32_64_USB/rpi-usb.h:
-/home/diego/Desktop/OStrich/Arm32_64_USB/emb-stdio.h:
