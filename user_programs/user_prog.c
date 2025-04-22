f61752f1f1e6a7f34a90a78e5a1d3f6f000a4b19
Sahaj Bhakta <85909754+BajuMcBites@users.noreply.github.com>
2025-04-22 02:00:42 -0500

    prevented race condition with removal and adding of locations

--- user_programs/user_prog.c
+++ user_programs/user_prog.c
@@ -2,18 +2,38 @@
 #include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
-    for (int i = 0; i < 20; i++) {
-        int c = fork();
-        if (c == 0) {
-            char* str= "0\0";
-            str[0] += i;
-            char* nargv[2] = {str, "\0"};
-            execve("exit", nargv, 0);
-            _exit(10200);
-        } else {
-            int sig = 0;
-            continue;
+    int c = fork();
+    fork();
+    fork();
+
+    int check_copy = 0;
+
+    if (c == 0) {
+        if (check_copy != 0) {
+            (*NULL);
         }
+        check_copy = 1;
+    } else {
+        if (check_copy != 0) {
+            (*NULL);
+        }
+        check_copy = 1;
     }
-    _exit(0);
+
+    check_copy = 0;
+
+    int d = fork();
+    int e = fork();
+
+    check_copy = 1;
+
+    if (check_copy != 1) {
+        (*NULL);
+    }
+
+    check_copy = 0;
+    char* str= "0\0";
+    str[0] += getpid();
+    char* nargv[2] = {str, "\0"};
+    execve("exit", nargv, 0);
 }
