b9b48784536bbd278cef4759d9100efe3c771e09
Jeffrey Zhou <zhoujeffrey@yahoo.com>
2025-04-27 21:06:01 -0500

    added set perms

--- include/user_permissions.h
+++ include/user_permissions.h
@@ -3,9 +3,15 @@
 
 #include "stdint.h"
 
+struct Permissions {
+    bool can_change_perms;
+    uint64_t newlib_syscall_perms;
+} typedef Permissions;
+
 void init_permissions();
 void set_user_id(uint32_t id);
 bool check_newlib_syscall_perm(uint32_t user, uint32_t syscall_num);
 uint32_t get_cur_user();
+void set_perms(uint32_t user, struct Permissions *perms);
 
 #endif
--- src/user_permissions.cpp
+++ src/user_permissions.cpp
@@ -12,25 +12,33 @@ static bool uint32_t_equals(uint32_t elem, uint32_t elem2) {
     return elem == elem2;
 }
 
-struct Permissions {
-    uint64_t newlib_syscall_perms;
-} typedef Permissions;
-
 HashMap<uint32_t, Permissions*> *perm_mappings;
 
 void init_permissions() {
     perm_mappings = new HashMap<uint32_t, struct Permissions *>(uint32_t_hash, uint32_t_equals);
     struct Permissions *root = (struct Permissions*) kmalloc(sizeof(struct Permissions));
     root->newlib_syscall_perms = ~(0);
+    root->can_change_perms = false;
     perm_mappings->put(0, root);
 
     struct Permissions *user = (struct Permissions*) kmalloc(sizeof(struct Permissions));
     user->newlib_syscall_perms = 0;
+    user->can_change_perms = false;
     perm_mappings->put(404, user);
 }
 
 void set_user_id(uint32_t id) {
     cur_user = id;
+}
+
+void set_perms(uint32_t user, struct Permissions *perms) {
+    if (!perm_mappings->get(cur_user)->can_change_perms) {
+        return;
+    }
+
+    struct Permissions *old = perm_mappings->get(user);
+    kfree(old);
+    perm_mappings->put(user, perms);
 }
 
 uint32_t get_cur_user() {
